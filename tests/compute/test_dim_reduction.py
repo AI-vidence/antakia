e3367b878ad13ef27f88f7a4ebaee0cee7bbe613
Pierre Hulot <pierre@storelift.co>
2023-11-28 16:15:02 +0100

    test update

--- tests/compute/test_dim_reduction.py
+++ tests/compute/test_dim_reduction.py
@@ -6,9 +6,10 @@ from antakia.compute.dim_reduction.dim_reduction import compute_projection
 
 
 def test_dim_reduction():
-    X = pd.DataFrame(np.random.random((100, 5)))
+    X = pd.DataFrame(np.random.random((100, 5)), index=np.random.choice(np.random.randint(1000, size=200), size=100))
     y = X.sum(axis=1)
 
     for dim_method in DimReducMethod.dimreduc_methods_as_list():
+        print(DimReducMethod.dimreduc_method_as_str(dim_method))
         compute_projection(X, y, dim_method, 2, lambda x, y, z: None)
         compute_projection(X, y, dim_method, 3, lambda x, y, z: None)
