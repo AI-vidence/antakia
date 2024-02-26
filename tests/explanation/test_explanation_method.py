0f8e5e977aba69c729eb915025db8a0ee480e659
walid <walidamrouche@yahoo.fr>
2024-02-26 18:05:23 +0100

    test des méthodes statiques sans instancier

--- tests/explanation/test_explanation_method.py
+++ tests/explanation/test_explanation_method.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 import numpy as np
 import pytest
 
+from antakia.explanation.explanation_method import ExplanationMethod
 from antakia.utils.dummy_datasets import generate_corner_dataset
 from tests.utils_fct import EMPTYExplanation, DummyModel, dummy_callable
 
@@ -18,24 +19,24 @@ class TestExplanationMethod(TestCase):
         assert exp_meth.explanation_method == 1
         np.testing.assert_array_equal(exp_meth.X, self.X)
 
-    def test_is_valid_explanation_method(self):
-        exp_meth = EMPTYExplanation(self.X, self.y, self.model, self.callable)
-        assert (exp_meth.is_valid_explanation_method(1))
 
-    def test_explanation_methods_as_list(self):
-        exp_meth = EMPTYExplanation(self.X, self.y, self.model, self.callable)
-        assert exp_meth.explanation_methods_as_list() == [1, 2]
+def test_is_valid_explanation_method():
+    assert (ExplanationMethod.is_valid_explanation_method(1))
 
-    def test_explain_method_as_str(self):
-        exp_meth = EMPTYExplanation(self.X, self.y, self.model, self.callable)
-        assert exp_meth.explain_method_as_str(1) == 'SHAP'
-        assert exp_meth.explain_method_as_str(2) == 'LIME'
-        with pytest.raises(ValueError):
-            exp_meth.explain_method_as_str(3)
 
-    def test_explain_method_as_int(self):
-        exp_meth = EMPTYExplanation(self.X, self.y, self.model, self.callable)
-        assert exp_meth.explain_method_as_int('shap') == 1
-        assert exp_meth.explain_method_as_int('Lime') == 2
-        with pytest.raises(ValueError):
-            exp_meth.explain_method_as_int('shapp')
+def test_explanation_methods_as_list():
+    assert ExplanationMethod.explanation_methods_as_list() == [1, 2]
+
+
+def test_explain_method_as_str():
+    assert ExplanationMethod.explain_method_as_str(1) == 'SHAP'
+    assert ExplanationMethod.explain_method_as_str(2) == 'LIME'
+    with pytest.raises(ValueError):
+        ExplanationMethod.explain_method_as_str(3)
+
+
+def test_explain_method_as_int():
+    assert ExplanationMethod.explain_method_as_int('shap') == 1
+    assert ExplanationMethod.explain_method_as_int('Lime') == 2
+    with pytest.raises(ValueError):
+        ExplanationMethod.explain_method_as_int('shapp')
