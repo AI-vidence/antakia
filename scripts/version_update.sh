f9d86a196028411b418350b582070955a9998ec6
pierre <pierre@ai-vidence.com>
2024-03-05 19:53:09 +0100

    version update

--- scripts/version_update.sh
+++ scripts/version_update.sh
@@ -1,30 +1,33 @@
 cd ../antakia-core
-atkc= $(echo $(poetry version) | awk '{print $2}')
+atkc=$(echo $(poetry version) | awk '{print $2}')
 cd ../antakia
-atk= $(echo $(poetry version) | awk '{print $2}')
+atk=$(echo $(poetry version) | awk '{print $2}')
 
-if [ "$atkc" != "$atk" ]; then
+if [ "$atkc" != "$atk" ]
+then
    echo 'version do not match'
    exit 1
+else
+   echo 'same version'
 fi
 
 cd ../antakia-core
 git stash
 git checkout -f dev
 git pull
-new_v= $(poetry version $0)
-new_v= $(echo $new_v | awk '{print $6}')
+new_v=$(poetry version $0)
+new_v=$(echo $new_v | awk '{print $6}')
 git add pyproject.toml
 git commit -m 'version $0 -> $new_v'
 git stash pop
 
 cd ../antakia
-poetry version $0
 git stash
 git checkout -f dev
 git pull
-new_v= $(poetry version $0)
-new_v= $(echo $new_v | awk '{print $6}')
+new_v=$(poetry version $0)
+new_v=$(echo $new_v | awk '{print $6}')
+poetry run python scripts/version_sync
 git add pyproject.toml
 git commit -m 'version $0 -> $new_v'
 git stash pop
